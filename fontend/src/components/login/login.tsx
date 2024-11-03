4325c6ef3b360bf24ccb4697084d8dcc1454dbc0
patipanbank <152005131+patipanbank@users.noreply.github.com>
2024-11-03 15:38:09 +0700

    Update login.tsx

--- fontend/src/components/login/login.tsx
+++ fontend/src/components/login/login.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useState } from 'react';
-import { Card, CardHeader, CardTitle, CardContent, CardFooter } from '@/components/ui/card';
+import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Eye, EyeOff } from 'lucide-react';
